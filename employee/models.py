2ebbebfed5df91afc61b798f3024a37d81efa646
AndrewBenson98 <andrewbenson123@gmail.com>
2021-09-13 18:58:31 -0400

    added feature to shrink large profile pictures

--- employee/models.py
+++ employee/models.py
@@ -4,6 +4,7 @@ from django.db.models.fields import CharField, EmailField, NullBooleanField
 from django.http import request
 from employee.utils import create_new_ref_number, generateTempPassword
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Department(models.Model):
@@ -43,6 +44,12 @@ class Employee(models.Model):
             self.user = User.objects.get(username=self.username)
         
         super(Employee, self).save(*args,**kwargs)
+        img = Image.open(self.image.path)
+        
+        if img.height >300 or img.width >300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
     def __str__(self):
         return f"{self.fName} {self.lName}"
