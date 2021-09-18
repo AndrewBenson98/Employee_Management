507bad71ba446a5f915c04768e29a99c0026b087
AndrewBenson98 <andrewbenson123@gmail.com>
2021-09-18 15:42:41 -0400

    removed default =1  from 0004 migration

--- employee/migrations/0004_auto_20210908_1644.py
+++ employee/migrations/0004_auto_20210908_1644.py
@@ -14,8 +14,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='employee',
             name='id',
-            field=models.BigAutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
-            
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            preserve_default=False,
         ),
         migrations.AlterField(
             model_name='employee',
